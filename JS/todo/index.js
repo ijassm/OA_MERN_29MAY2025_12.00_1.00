9cdd5ef58534f9daab689eba0b17f2377c45a9bd
ijassm <ijass.ocean@gmail.com>
2025-08-29 13:10:20 +0530

    Add this.html with arrow function examples and task management improvements in index.js

--- JS/todo/index.js
+++ JS/todo/index.js
@@ -2,11 +2,16 @@ const doc = document;
 const body = doc.body;
 let _id = 0;
 
+const clearInput = () => {
+    doc.querySelector("#taskInput").value = "";
+}
+
 doc.querySelector("#taskInput").addEventListener("keydown", (event) => {
     // console.log(event)
     if (event.key === "Enter") {
         console.log("✅entered");
         addTask();
+        clearInput();
     }
 })
 
@@ -15,22 +20,46 @@ const addTask = () => {
     const newTaskUI = taskUI(task)
     const taskList = doc.querySelector("#taskList");
     taskList.innerHTML += newTaskUI
+    clearInput();
     console.log("✅ New Task Added : ", task);
 }
 
 
-const deleteTask = (e) => {
-    console.log(e);
+const deleteTask = (id) => {
+    const taskList = doc.querySelector("#taskList");
+    const task = doc.getElementById(`task_${id}`);
+    taskList.removeChild(task);
+    console.log("✅ Task Deleted : ", task);
+}
+
+
+const editTask = (id) => {
+    const taskInput = doc.querySelector(`#task_${id} > input`);
+    const editBtn = doc.querySelector(`#task_${id} > .btns > .edit`);
+    if (editBtn.textContent === "Update") {
+        // taskInput.value = taskInput.value.trim();
+        taskInput.setAttribute("value", doc.querySelector(`#task_${id} > input`).value);
+
+        taskInput.setAttribute("readonly", true);
+        editBtn.textContent = "Edit";
+    } else {
+        taskInput.removeAttribute("readonly");
+        editBtn.textContent = "Update";
+    }
+
+    console.log(editBtn);
+
+    // editBtn.setAttribute("onclick", `saveTask(${id})`);
 }
 
 
 const taskUI = (value) => {
     const id = _id++;
-    return `<li class="task">
+    return `<li class="task" id="task_${id}">
           <input id="${id}" type="text" readonly value="${value.toString()}" />
           <div class="btns">
-            <button id="${id}">Edit</button>
-            <button id="${id}" class="delete" onclick="${deleteTask(this)}">Delete</button>
+            <button class="edit" onclick="editTask(${id})">Edit</button>
+            <button class="delete" onclick="deleteTask(${id})">Delete</button>
           </div>
         </li>`
 }
